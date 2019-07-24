3482124dea0f2edc7d9c29164ec8d9cf3790b4b6
lvandenbrink <lvdbrink@sphereon.com>
2019-07-24 12:28:09 +0200

    log fix the arguments

--- EventRouter/log/logger.go
+++ EventRouter/log/logger.go
@@ -27,30 +27,30 @@ func SetLogger(l *log.Logger) {
 
 func Info(format string, v ...interface{}) {
 	if level >= I {
-		logger.Printf("[INFO] "+format, v)
+		logger.Printf("[INFO] "+format, v...)
 	}
 }
 
 func Debug(format string, v ...interface{}) {
 	if level >= D {
-		logger.Printf("[DEBUG] "+format, v)
+		logger.Printf("[DEBUG] "+format, v...)
 	}
 }
 
 func Warn(format string, v ...interface{}) {
 	if level >= W {
-		logger.Printf("[WARN] "+format, v)
+		logger.Printf("[WARN] "+format, v...)
 	}
 }
 
 func Error(format string, v ...interface{}) {
 	if level >= E {
-		logger.Printf("[ERROR] "+format, v)
+		logger.Printf("[ERROR] "+format, v...)
 	}
 }
 
 func Fatal(format string, v ...interface{}) {
 	if level >= F {
-		logger.Fatalf("[FATAL] "+format, v)
+		logger.Fatalf("[FATAL] "+format, v...)
 	}
 }
