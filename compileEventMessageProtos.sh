8fc28228c6ba4e47d445d5333d28c2b937568daa
sanderPostma <spostma@sphereon.com>
2019-07-29 11:39:58 +0200

    Updated proto buffers

--- compileEventMessageProtos.sh
+++ compileEventMessageProtos.sh
@@ -1,8 +1,12 @@
-protoc -I=. -I=$GOPATH/src -I=$GOPATH/src/github.com/gogo/protobuf/protobuf \
+protoc -I=. -I=$GOPATH/src -I=$GOPATH/src/github.com/gogo/protobuf/protobuf  -I=EventRouter/events/eventmessages \
 --gofast_out=\
 Mgoogle/protobuf/any.proto=github.com/gogo/protobuf/types,\
 Mgoogle/protobuf/duration.proto=github.com/gogo/protobuf/types,\
 Mgoogle/protobuf/struct.proto=github.com/gogo/protobuf/types,\
 Mgoogle/protobuf/timestamp.proto=github.com/gogo/protobuf/types,\
 Mgoogle/protobuf/wrappers.proto=github.com/gogo/protobuf/types:. \
-./EventRouter/events/eventmessages/factomEvents.proto
+generalTypes.proto \
+factoidBlock.proto \
+adminBlock.proto \
+entryCredit.proto \
+factomEvents.proto
