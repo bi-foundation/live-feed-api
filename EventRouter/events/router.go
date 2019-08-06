8864fd248560b776f3aa0465a628ff915b130cab
sanderPostma <spostma@sphereon.com>
2019-08-06 12:03:02 +0200

    Post-merge fixes

--- EventRouter/events/router.go
+++ EventRouter/events/router.go
@@ -1,8 +1,8 @@
 package events
 
 import (
-	"live-api/EventRouter/events/eventmessages"
-	"live-api/EventRouter/log"
+	"github.com/FactomProject/live-api/EventRouter/events/eventmessages"
+	"github.com/FactomProject/live-api/EventRouter/log"
 )
 
 type EventRouter struct {
@@ -22,20 +22,15 @@ func (evr *EventRouter) handleEvents() {
 		log.Debug("handle event: %v", factomEvent)
 		switch factomEvent.Value.(type) {
 		case *eventmessages.FactomEvent_AnchorEvent:
-			log.Info("Received AnchoredEvent: %v", factomEvent.GetAnchorEvent())
-			log.Info("Received AnchoredEvent with event source ", factomEvent.GetEventSource().String(), factomEvent.GetAnchorEvent())
+			log.Info("Received AnchoredEvent with event source %v: %v", factomEvent.GetEventSource(), factomEvent.GetAnchorEvent())
 		case *eventmessages.FactomEvent_CommitChain:
-			log.Info("Received CommitChain: %v", factomEvent.GetCommitChain())
-			log.Info("Received CommitChain with event source ", factomEvent.GetEventSource().String(), factomEvent.GetCommitChain())
+			log.Info("Received CommitChain with event source %v: %v", factomEvent.GetEventSource(), factomEvent.GetCommitChain())
 		case *eventmessages.FactomEvent_CommitEntry:
-			log.Info("Received CommitEntry: %v", factomEvent.GetCommitEntry())
-			log.Info("Received CommitEntry with event source ", factomEvent.GetEventSource().String(), factomEvent.GetCommitEntry())
+			log.Info("Received CommitEntry with event source %v: %v", factomEvent.GetEventSource(), factomEvent.GetCommitEntry())
 		case *eventmessages.FactomEvent_RevealEntry:
-			log.Info("Received FactomEvent_RevealEntry: %v", factomEvent.GetRevealEntry())
-			log.Info("Received FactomEvent_RevealEntry with event source ", factomEvent.GetEventSource().String(), factomEvent.GetRevealEntry())
+			log.Info("Received FactomEvent_RevealEntry with event source %v: %v", factomEvent.GetEventSource(), factomEvent.GetRevealEntry())
 		case *eventmessages.FactomEvent_NodeMessage:
-			log.Info("Received FactomEvent_NodeMessage: %v", factomEvent.GetNodeMessage())
-			log.Info("Received FactomEvent_NodeMessage with event source ", factomEvent.GetEventSource().String(), factomEvent.GetNodeMessage())
+			log.Info("Received FactomEvent_NodeMessage with event source %v: %v", factomEvent.GetEventSource(), factomEvent.GetNodeMessage())
 		}
 	}
 }
