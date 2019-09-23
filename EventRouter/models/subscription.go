7cbc0121ec5a9d41e2953c7f365b9edae6f6bb8e
lvandenbrink <lvdbrink@sphereon.com>
2019-09-23 11:44:35 +0200

    add readonly attributes to subscription fields

--- EventRouter/models/subscription.go
+++ EventRouter/models/subscription.go
@@ -4,7 +4,7 @@ package models
 type Subscription struct {
 
 	// The id of the subscription.
-	ID string `json:"id" binding:"required"`
+	ID string `json:"id" readonly:"true"`
 
 	// The callback endpoint to receive the events.
 	CallbackURL string `json:"callbackUrl" binding:"required" example:"https://server.com/events"`
@@ -16,10 +16,10 @@ type Subscription struct {
 	CallbackType CallbackType `json:"callbackType" binding:"required" example:"HTTP" enums:"HTTP,BEARER_TOKEN,BASIC_AUTH"`
 
 	// Status of subscription. Normally a subscription is active. When events fail to be delivered the subscription will be suspended. The subscription can become active again by updating the subscription. When the subscription is suspended, the error information is set in the info field.
-	SubscriptionStatus SubscriptionStatus `json:"status" example:"ACTIVE" enums:"ACTIVE,SUSPENDED"`
+	SubscriptionStatus SubscriptionStatus `json:"status" example:"ACTIVE" enums:"ACTIVE,SUSPENDED" readonly:"true"`
 
 	// Information of the subscription. An information message can be for example about why the subscription is suspended.
-	SubscriptionInfo string `json:"info"`
+	SubscriptionInfo string `json:"info" readonly:"true"`
 
 	// The emitted event can be filter to receive not all data from an event type. Subscribe on one or more event types. For every event type a filtering can be defined.
 	Filters map[EventType]Filter `json:"filters"`
